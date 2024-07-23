005d723630e3c4ca4752f0a67d4e08cef4d05ddb
KavitaJogi <k.p.jogi89@gmail.com>
2024-07-23 15:38:27 +0530

    added

---
+++ Assignments/Modual1/stringvalidation.py
@@ -0,0 +1,24 @@
+name=input("Enter Your Name: ")
+if name.isalpha():
+    print("success")
+ 
+age=input("Enter Your Age: ")
+if age.isdigit():
+    print("Suceess")
+
+email=input("Enter Your Email Address: ")
+if email.islower():
+    print("Suceess")
+
+pwd=input("Enter Your Password: ")
+cnfpwd=input("Enter Your Confirm Password: ")
+if pwd==cnfpwd:
+    print("Sucess")
+else:
+    print("False")
+
+mbno=input("Enter Your Mobile Number: ")
+if mbno.isdigit(): 
+    print("Success")
+
+
