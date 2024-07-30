c7a49059de79d2948c845f645b7c26343c5d4c06
KavitaJogi <k.p.jogi89@gmail.com>
2024-07-30 17:58:00 +0530

    new added

---
+++ Assignments/Modual2/Length_of_word.py
@@ -0,0 +1,12 @@
+n=int(input("How many word you want to enter? "))
+ip_no=[]
+for i in range(n):
+    word=input("Enter Word: ")
+    ip_no.append(word)
+maxlen=len(ip_no[0])
+temp=ip_no[0]
+for j in ip_no:
+    if len(j) > maxlen:
+        maxlen=len(j)
+        temp=j
+print(f"word with the longest length is: {temp}")    
