2151b03f5aeb1f1e907519498c64ef04b8eacea0
KavitaJogi <k.p.jogi89@gmail.com>
2024-08-08 18:04:18 +0530

    new added

---
+++ Assignments/Modual3/compare_two_list.py
@@ -0,0 +1,25 @@
+mylist1=[]
+mylist2=[]
+
+n1=int(input("Enter size for list1: "))
+
+for i in range(n1):
+    add=input("Enter value in list1: ")
+    mylist1.append(add)
+print(mylist1)
+
+n2=int(input("Enter size for list2: "))
+
+for j in range(n2):
+    add=input("Enter value in list2: ")
+    mylist2.append(add)
+print(mylist2)
+
+set1=set(mylist1)
+set2=set(mylist2)
+
+if set1.intersection(set2):
+    print("true")
+else:
+    print("error")
+
