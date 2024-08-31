eee56a83d819572906bff61320e89c0565245cfc
KavitaJogi <k.p.jogi89@gmail.com>
2024-08-31 17:12:46 +0530

    modified

--- Assignments/Modual3/list_empty.py
+++ Assignments/Modual3/list_empty.py
@@ -3,8 +3,8 @@ n=int(input("Enter a list size: "))
 for i in range(n):
     add=input("Enter value in list: ")
     mylist.append(add)
-    length=len(mylist)
-    if length!=1:
-        print("List is empty")
-    else:
-        print("List is not empty")
+length=len(mylist)
+if length==0:
+    print("List is empty")
+else:
+    print("List is not empty")
---
+++ Assignments/Modual3/remove_empty_tuple_from_listof_tuple.py
@@ -0,0 +1,10 @@
+mylist=[]
+temp=[]
+n=int(input("how many tuple you want to enter : "))
+
+for i in range(n):
+    add=int(input("Enter size : "))
+    for j in range(add):
+        mylist.append(add)
+
+tuple=tuple(mylist)
