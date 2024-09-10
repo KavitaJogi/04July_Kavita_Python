223ea84d8ec06ecef54dc3c2b576d9cde9152a54
KavitaJogi <k.p.jogi89@gmail.com>
2024-09-10 18:06:47 +0530

    new added

---
+++ Assignments/Modual3/check_presence_of_key_in_dictionary.py
@@ -0,0 +1,15 @@
+mydict={}
+
+n=int(input("Enter Number of Dictionary Pairs : "))
+
+for i in range(n):
+    key=input(f"Enter {i+1} key : ")
+    value=input(f"Enter {i+1} value : ")
+    mydict[key]=value
+
+check=input("Enter Key Name : ")
+
+if check in mydict:
+    print(f"Key '{check}' is already exist.")
+else:
+    print(f"Key '{check}' is not exist.")
