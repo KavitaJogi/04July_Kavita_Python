9d31eaef143ddc6c4a52d6a82597821975ed0213
KavitaJogi <k.p.jogi89@gmail.com>
2024-09-24 16:22:19 +0530

    new added

---
+++ Assignments/Modual3/calculate_area_of_trapezoid.py
@@ -0,0 +1,5 @@
+base1 = float(input("Enter the Base 1 Value :"))
+base2 = float(input("Enter the Base 2 Value :"))
+height = float(input("Enter Height of Trapezoid: "))
+area = ((base1 + base2) / 2) * height
+print("Area : ", area)
